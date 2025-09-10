9c573ec574eb10725596c2ccf60041392baef937
Eric Telkkälä <52538447+erictelkkala@users.noreply.github.com>
2025-09-10 19:54:31 +0300

    Add method for getting episode number from media file

--- src/main/java/erictelkkala/f1_metadata_generator_for_jellyfin/localFiles/MediaFile.java
+++ src/main/java/erictelkkala/f1_metadata_generator_for_jellyfin/localFiles/MediaFile.java
@@ -1,6 +1,8 @@
 package erictelkkala.f1_metadata_generator_for_jellyfin.localFiles;
 
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public record MediaFile(File file) {
@@ -13,6 +15,19 @@ public record MediaFile(File file) {
             return Stream.of(files).filter(File::isFile).anyMatch(file1 -> file1.getName().equals(fileName + ".nfo"));
         } else {
             return false;
+        }
+    }
+
+    public String episodeNumber() {
+        final String fileName = file.getName();
+        final Pattern pattern = Pattern.compile("\\d{2} - ");
+        final Matcher matcher = pattern.matcher(fileName);
+        if (matcher.find()) {
+            final int indexAfter = matcher.end();
+            return fileName.substring(indexAfter - 5, indexAfter - 3);
+
+        } else {
+            throw new IllegalArgumentException("No episode number found in file name: " + fileName);
         }
     }
 }
--- src/test/java/erictelkkala/f1_metadata_generator_for_jellyfin/localFiles/MediaFileTest.java
+++ src/test/java/erictelkkala/f1_metadata_generator_for_jellyfin/localFiles/MediaFileTest.java
@@ -35,4 +35,37 @@ final class MediaFileTest {
 
         Assertions.assertFalse(mediaFile1.hasNfo());
     }
+
+    @Test
+    @DisplayName("episodeNumber works with season plus episode format")
+    void episodeNumberWorksWithSeasonPlusEpisodeFormat(@TempDir Path tempDir) {
+        final Path mediaFile = Assertions.assertDoesNotThrow(() -> Files.createFile(Path.of(tempDir + "/Formula 1 - s2025e01 - Australian Grand Prix - Qualification.mkv.mkv")));
+        Assertions.assertNotNull(mediaFile);
+        final MediaFile mediaFile1 = new MediaFile(mediaFile.toFile());
+
+        final String expectedEpisodeNumber = "01";
+        Assertions.assertEquals(expectedEpisodeNumber, mediaFile1.episodeNumber());
+    }
+
+    @Test
+    @DisplayName("episodeNumber works with episode only format")
+    void episodeNumberWorksWithEpisodeOnlyFormat(@TempDir Path tempDir) {
+        final Path mediaFile = Assertions.assertDoesNotThrow(() -> Files.createFile(Path.of(tempDir + "/Formula 1 - e12 - Australian Grand Prix - Qualification.mkv.mkv")));
+        Assertions.assertNotNull(mediaFile);
+        final MediaFile mediaFile1 = new MediaFile(mediaFile.toFile());
+
+        final String expectedEpisodeNumber = "12";
+        Assertions.assertEquals(expectedEpisodeNumber, mediaFile1.episodeNumber());
+    }
+
+    @Test
+    @DisplayName("episodeNumber works with episode only format, without Formula 1 prefix")
+    void episodeNumberWorksWithEpisodeOnlyFormatWithoutFormula1Prefix(@TempDir Path tempDir) {
+        final Path mediaFile = Assertions.assertDoesNotThrow(() -> Files.createFile(Path.of(tempDir + "/e13 - Australian Grand Prix - Qualification.mkv.mkv")));
+        Assertions.assertNotNull(mediaFile);
+        final MediaFile mediaFile1 = new MediaFile(mediaFile.toFile());
+
+        final String expectedEpisodeNumber = "13";
+        Assertions.assertEquals(expectedEpisodeNumber, mediaFile1.episodeNumber());
+    }
 }
